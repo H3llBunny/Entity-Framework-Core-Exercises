0e7201cde22966c1aef91f832ea0b75ec94de811
H3llBunny <bogo.dishlyanov@gmail.com>
2024-02-22 21:17:19 +0200

    Added MongoDB exercise

---
+++ No-SQL/ArticlesMongoDBExercise/Article.cs
@@ -0,0 +1,29 @@
+﻿
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace ArticlesMongoDBExercise
+{
+    public class Article
+    {
+        [BsonElement("author")]
+        public string Author { get; set; }
+
+        [BsonElement("date")]
+        public string Date { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("rating")]
+        public int Rating { get; set; }
+    }
+
+    public class ArticlesData
+    {
+        [BsonElement("articles")]
+        public List<Article> Articles { get; set; }
+    }
+}
